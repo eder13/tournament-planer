b723db4bb6095aa82e02fb79225c28145d1a4f49
eder13 <simon.ranftl82@gmail.com>
2025-12-25 08:01:28 +0100

    updated database connection prisma client info check

--- server/src/db/prisma.ts
+++ server/src/db/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import Logger from '../helpers/logger';
 
 class Database {
     private static _instance: PrismaClient | null = null;
@@ -7,6 +8,14 @@ class Database {
     static getInstance() {
         if (!this._instance) {
             this._instance = new PrismaClient();
+            this._instance
+                .$connect()
+                .then(() => {
+                    Logger.info('Database is up and running...');
+                })
+                .catch((e) => {
+                    Logger.error(`Could not connect to Database: ${e}`);
+                });
         }
         return this._instance;
     }
